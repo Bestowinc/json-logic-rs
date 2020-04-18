d988061283d7b2c89e58b18edaf5a19d5e94bba3
Matthew Planchard <msplanchard@gmail.com>
2020-04-17 21:38:04 -0500

    ENH: add UnexpectedArgument error member

--- src/error.rs
+++ src/error.rs
@@ -8,14 +8,25 @@ use thiserror;
 pub enum Error {
     #[error("Invalid data - value: {value:?}, reason: {reason:?}")]
     InvalidData { value: Value, reason: String },
+
     #[error("Invalid rule - operator: '{key:?}', reason: {reason:?}")]
     InvalidOperation { key: String, reason: String },
+
     #[error("Invalid variable - '{value:?}', reason: {reason:?}")]
     InvalidVariable { value: Value, reason: String },
+
+    #[error("Invalid argument for '{operation}' - '{value:?}', reason: {reason}")]
+    InvalidArgument { value: Value, operation: String, reason: String },
+
     #[error("Invalid variable mapping - {0} is not an object.")]
     InvalidVarMap(Value),
+
     #[error("Encountered an unexpected error. Please raise an issue on GitHub and include the following error message: {0}")]
     UnexpectedError(String),
+
     #[error("Wrong argument count - expected: {expected:?}, actual: {actual:?}")]
-    WrongArgumentCount { expected: std::ops::Range<usize>, actual: usize },
+    WrongArgumentCount {
+        expected: std::ops::Range<usize>,
+        actual: usize,
+    },
 }
