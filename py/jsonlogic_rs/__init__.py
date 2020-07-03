b645fcac02d86fe5d4141d02e9a252271bb2cfad
Matthew Planchard <msplanchard@gmail.com>
2020-07-03 18:29:23 -0500

    ENH: sdist compatible python build, updated dir structure

--- py/jsonlogic_rs/__init__.py
+++ py/jsonlogic_rs/__init__.py
@@ -5,20 +5,20 @@ __all__ = (
     "jsonlogic_serialized",
 )
 
-import json
+import json as _json
 
-from .jsonlogic import jsonlogic as _jsonlogic
+from .jsonlogic import apply as _apply
 
 
-def jsonlogic(value, data=None, serializer=None, deserializer=None):
+def apply(value, data=None, serializer=None, deserializer=None):
     """Run JSONLogic on a value and some data."""
-    serializer = serializer if serializer is not None else json.dumps
-    deserializer = deserializer if deserializer is not None else json.loads
-    res = _jsonlogic(serializer(value), serializer(data))
+    serializer = serializer if serializer is not None else _json.dumps
+    deserializer = deserializer if deserializer is not None else _json.loads
+    res = _apply(serializer(value), serializer(data))
     return deserializer(res)
 
 
-def jsonlogic_serialized(value, data=None, deserializer=None):
+def apply_serialized(value: str, data: str = None, deserializer=None):
     """Run JSONLogic on some already serialized value and optional data."""
-    res = _jsonlogic(value, data if data is not None else "null")
+    res = _apply(value, data if data is not None else "null")
     return deserializer(res)
