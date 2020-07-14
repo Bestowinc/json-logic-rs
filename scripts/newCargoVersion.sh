ecde794976e9980eee7d06994fa4a733579226e5
Matthew Planchard <mplanchard@users.noreply.github.com>
2020-07-13 21:40:23 -0500

    FIX: Make checkNewCargoVersion.sh windows-compatible; fix secret injection (#14)
    * FIX: pass secrets through env
    
    * REF: improve readability of newCargoVersion.sh
    
    * TEST: debug new cargo version on windows
    
    * TEST: don't pull wheels for debugging
    
    * FIX: make newCargoVersion.sh POSIX compliant for windows
    
    * MAINT: more windows debugging
    
    * TEST: make version script work w/windows paths
    
    * CHORE: revert from debugging state

--- scripts/newCargoVersion.sh
+++ scripts/newCargoVersion.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CURRENT_VERSION=$(cargo pkgid | tr ':' ' ' | awk '{print $3}')
+CURRENT_VERSION=$(cargo pkgid | tr '#' '\n' | tail -n 1 | tr ':' ' ' | awk '{print $2}')
 
-RESP=$(curl 'https://crates.io/api/v1/crates/jsonlogic-rs' -H 'User-Agent: mplanchard_verison_check (msplanchard@gmail.com)' -H 'Accept: application/json' -H 'Cache-Control: max-age=0' -s)
+RESP=$(curl 'https://crates.io/api/v1/crates/jsonlogic-rs' -s \
+    -H 'User-Agent: mplanchard_verison_check (msplanchard@gmail.com)' \
+    -H 'Accept: application/json' \
+    -H 'Cache-Control: max-age=0')
 
-PREV_VERSION=$(echo "${RESP}" | tr ',' '\n' | grep newest_version | tr ':' ' ' | awk '{print $2}' | sed 's/"//g')
+PREV_VERSION=$(echo "${RESP}" \
+    | tr ',' '\n' \
+    | grep newest_version \
+    | tr ':' ' ' \
+    | awk '{print $2}' \
+    | sed 's/"//g')
 
 if [[ "${CURRENT_VERSION}" == "${PREV_VERSION}" ]]; then
     echo false
