338b70007aae0c6193ac2ad8471b774d8b79983a
Matthew Planchard <msplanchard@gmail.com>
2021-01-06 11:11:10 -0600

    MAINT: Add python 3.9 and AMD wheels
    Updates CI to build wheels for python 3.9 and AMD. Pulls in new
    manlyinux wheels that include python 3.9 and the manylinux wheel for
    AMD. Updates `build-wheels.sh` to include python 3.9 in the wheel
    generation script.

--- build-wheels.sh
+++ build-wheels.sh
@@ -6,23 +6,23 @@ set -ex
 
 cd /io
 
-curl https://sh.rustup.rs -sSf | \
-    sh -s -- --default-toolchain stable -y
+curl https://sh.rustup.rs -sSf |
+	sh -s -- --default-toolchain stable -y
 
 export PATH=/root/.cargo/bin:$PATH
 
 mkdir -p build && rm -rf build/*
 
-for PYBIN in /opt/python/{cp36-cp36m,cp37-cp37m,cp38-cp38}/bin; do
-    export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
+for PYBIN in /opt/python/{cp36-cp36m,cp37-cp37m,cp38-cp38,cp39-cp39}/bin; do
+	export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
 
-    "${PYBIN}/python" -m ensurepip
-    # Note: please change both here and in the makefile if specifying a particular
-    # version or removing the version pin.
-    "${PYBIN}/python" -m pip install -U setuptools wheel setuptools-rust==0.10.6
-    "${PYBIN}/python" setup.py bdist_wheel
+	"${PYBIN}/python" -m ensurepip
+	# Note: please change both here and in the makefile if specifying a particular
+	# version or removing the version pin.
+	"${PYBIN}/python" -m pip install -U setuptools wheel setuptools-rust==0.10.6
+	"${PYBIN}/python" setup.py bdist_wheel
 done
 
 for whl in dist/*.whl; do
-    auditwheel repair "$whl" -w dist/
+	auditwheel repair "$whl" -w dist/
 done
