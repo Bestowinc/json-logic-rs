b2d36d7ad79433a4d2e0ebe88e8ac50ec57ec220
Jonathan Stewmon <jonathan.stewmon@bestow.com>
2024-12-13 10:02:14 -0600

    add support for python 3.13; drop support for 3.8

--- build-wheels.sh
+++ build-wheels.sh
@@ -13,7 +13,7 @@ export PATH=/root/.cargo/bin:$PATH
 
 mkdir -p build && rm -rf build/*
 
-for PYBIN in /opt/python/{cp38-cp38,cp39-cp39,cp310-cp310,cp311-cp311,cp312-cp312}/bin; do
+for PYBIN in /opt/python/{cp39-cp39,cp310-cp310,cp311-cp311,cp312-cp312,cp-313-cp313}/bin; do
 	export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
 
 	"${PYBIN}/python" -m ensurepip
